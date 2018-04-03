886b8c9ceee6be293c623ac0ee0cd4bfafc29005
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2018-04-04 00:55:05 +0530

    create controller and Ihostingenvironment property

---
+++ src/Services/ProductCatelogApi/Controllers/PicController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatelogApi.Controllers
+{
+    using Microsoft.AspNetCore.Hosting;
+
+    [Produces("application/json")]
+    [Route("api/pic")]
+    public class PicController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+
+        public PicController(IHostingEnvironment env)
+        {
+            this._env = env;
+        }
+
+        public IActionResult GetImage(int id)
+        {
+
+        }
+    }
+}
