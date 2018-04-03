b3d63ae3844187c7a744161eeab14260e2af9732
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2018-04-04 00:39:21 +0530

    seeding data

--- src/Services/ProductCatelogApi/Program.cs
+++ src/Services/ProductCatelogApi/Program.cs
@@ -10,11 +10,32 @@ using Microsoft.Extensions.Logging;
 
 namespace ProductCatelogApi
 {
+    using Microsoft.Extensions.DependencyInjection;
+
+    using ProductCatelogApi.Data;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<CatalogContext>();
+                    CatalogSeed.SeedAsync(context).Wait();
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured while seeding database");
+                }
+
+                host.Run();
+            }  
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
